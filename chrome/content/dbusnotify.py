48ff66e4515eecd10126a6f9525fa2cf16491873
Abhishek Mukherjee <linkinpark342@gmail.com>
2008-12-07 14:39:00 -0500

    Revert "Broke build, should do work in a branch"
    This reverts commit 21b95712459d98cafd2113a3d67473bc31cba9af.

--- chrome/content/dbusnotify.py
+++ chrome/content/dbusnotify.py
@@ -13,120 +13,134 @@ which was used extensively as an example for this
 import dbus
 import logging
 import gobject
-import sys, os
-from subprocess import call
 
-# Use gtk's main loop... since firefox needs it anyways...
 from  dbus.mainloop.glib import DBusGMainLoop
 DBusGMainLoop(set_as_default=True)
-LOOP = gobject.MainLoop()
 
 # Galago spec
 NOTIFICATIONS_OBJECT = 'org.freedesktop.Notifications'
 NOTIFICATIONS_PATH = '/org/freedesktop/Notifications'
 NOTIFICATIONS_INTERFACE = 'org.freedesktop.Notifications'
-
-# Message Properties
-APP_NAME = "Firefox"
-APP_ICON = "firefox"
-SUMMARY = "Firefox Download Complete"
-BODY = "%s has completed downloading"
-ACTIONS = ["open", "Open File", "opendir", "Open Directory"]
-HINTS = {"category": "transfer.complete"}
+CLOSED_REASON = ["", "expired", "dismissed", "CloseNotification", "undefined"]
 
 SESSION_BUS = dbus.SessionBus()
-assert(SESSION_BUS != None)
+if SESSION_BUS == None:
+    logging.critical("Could not connect to DBus Session")
+    exit(1)
 
-class FirefoxDbus(object):
+class GalagoNotification(object):
     """
-    Creates a message from Firefox to a galago compliant notification library
+    A message from Firefox to a Galago compliant notification library.
     """
-    def __init__(self, title, location):
-        self.id = id
-        self.title = title
-        self.location = location
-        self.notification_id = None
-    def downloadComplete(self, actionsEnabled=True):
+    def __init__(self, summary,
+            body=None,
+            appname=None,
+            appicon=None,
+            actions=None,
+            hints=None,
+            actions_order=None,
+            default_action=None,
+            closed_action=None):
         """
-        Sends a signal that download for title has completed
+        Instantiates an object for sending a message. You must call send() when
+        you  actually want to send the message.
+        summary
+        body    : pretty self explanatory
+        action  : A dictionary of {string, callable} where string is the text
+                  that should be displayed on the button and callable is a
+                  function that takes no arguments
+        hints   : directly passed to the notification server, see
+                  http://www.galago-project.org/specs/notification/0.9/x344.html
+        actions_order : If given, this should be a list of the keys of action in
+                        the order you would like them displayed
+        default_action: Default action to perform if the message is just
+                        clicked
+        closed_action : callable if the notification is closed, should take one
+                        one argument, which will be a string from CLOSED_REASON
         """
+        self.summary = summary
+        self.body = body
+        self.appname = (appname or "")
+        self.appicon = (appicon or "")
+        self.actions = (actions or {})
+        self.actions_order = (actions_order or 
+                              [ action for action in self.actions.keys() ])
+        assert len(self.actions) == len(self.actions_order)
+        self.default = default_action
+        self.hints = (hints or {"":0})
+        self.closed_action = closed_action
+        self._notification_id = None
+        self._loop = None
+    def send(self):
+        """ Sends notification to libnotify """
         notif = SESSION_BUS.get_object(NOTIFICATIONS_OBJECT, NOTIFICATIONS_PATH)
-        if actionsEnabled:
-            actions = ACTIONS
-        else:
-            actions = [""]
-
-        self.notification_id = notif.Notify(APP_NAME,
-                dbus.UInt32(0),
-                APP_ICON,
-                SUMMARY,
-                BODY % self.title,
-                actions,
-                HINTS,
+        # Get the actions into list format
+        actions = [ x for x in enumerate(self.actions_order) ]
+        actions_flat = []
+        for act in actions:
+            actions_flat.append(str(act[0]))
+            actions_flat.append(act[1])
+        notif_id = self._notification_id or 0
+        self._notification_id = notif.Notify(self.appname,
+                notif_id, self.appicon, self.summary,
+                self.body, actions_flat, self.hints,
                 dbus.Int32(-1),
                 dbus_interface=NOTIFICATIONS_INTERFACE)
 
         # Connect the signals for actions
-        notif.connect_to_signal("ActionInvoked", self.signalHandler,
+        notif.connect_to_signal("ActionInvoked", self._action_invoked,
                 dbus_interface=NOTIFICATIONS_INTERFACE)
 
         notif.connect_to_signal("NotificationClosed",
-                self.notificationClosedHandler,
+                self._notification_closed_handler,
                 dbus_interface=NOTIFICATIONS_INTERFACE)
 
-    def notificationClosedHandler(self, id):
+        # Only watch if we have actions
+        self._watch_for_signals()
+
+    def _notification_closed_handler(self, notif_id, reason):
         """
         Callback for a closed notification, used to end the gobject loop
         """
-        if id == self.notification_id:
-            LOOP.quit()
+        if notif_id == self._notification_id:
+            self._loop.quit()
+        if self.closed_action:
+            assert reason > 0
+            self.closed_action(CLOSED_REASON[reason])
 
-    def signalHandler(self, id, key):
+    def _action_invoked(self, notif_id, key):
         """
         Handler for when an action is invoked on the notification May not be
         relevant for all notification daemons
         """
-        if id == self.notification_id:
-            if key == ACTIONS[0]:
-                self.openFile()
-            elif key == ACTIONS[2]:
-                self.openDir()
-
-    def openFile(self):
+        if notif_id == self._notification_id:
+            if key == "default":
+                if self.default:
+                    self.actions[self.default]()
+            else:
+                self.actions[self.actions_order[int(key)]]()
+    def _watch_for_signals(self):
         """
-        Opens the file using xdg-open. You need xdg-utils for this to work.
+        Called so that our program does not end until we get a
+        closed_notification
         """
-        os.execlp("xdg-open", "xdg-open", self.location)
-    def openDir(self):
-        """
-        Opens the file's directory using xdg-open. You need xdg-utils for this
-        to work.
-        """
-        os.execlp("xdg-open", "xdg-open", os.path.dirname(self.location))
+        if self._loop == None:
+            self._loop = gobject.MainLoop()
+        self._loop.run()
 
 def main():
+    """Takes two arguments from sys.argv and creates a notification with argv[1]
+    as the summary and argv[2] as the body"""
+    import sys
     if len(sys.argv) != 3:
-        logging.warning("%s called with invalid number of arguments" % sys.argv[0])
+        logging.warning("%s called with invalid number of arguments" %
+                                                        sys.argv[0])
         logging.warning("Arguments were: %s" % sys.argv)
         sys.exit(1)
 
-    # Find out if we have ability to open files using portland project
-    try:
-        retval = call(["xdg-open", "--version"])
-    except:
-        retval = -1
-
-    if retval == 0:
-        actionsEnabled = True
-    else:
-        actionsEnabled = False
-
     # open notification
-    f = FirefoxDbus(title=sys.argv[1], location=sys.argv[2])
-    f.downloadComplete(actionsEnabled)
-
-    # start signal handler loop
-    LOOP.run()
+    notif = GalagoNotification(sys.argv[1], sys.argv[2])
+    notif.send()
 
 if __name__ == "__main__":
     main()
